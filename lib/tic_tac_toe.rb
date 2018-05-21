45c348a13d7243a01b163ace701d4fe5c7b869d6
Alex Zhang <alexgzhang92@gmail.com>
2018-05-21 00:40:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,6 +69,9 @@ class TicTacToe
     end
   end
   
-  
+  def won?
+    WIN_COMBINATIONS.detect {|combo|
+      @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == board[combo[2]] && position_taken?(combo[0])}
+  end
   
 end
