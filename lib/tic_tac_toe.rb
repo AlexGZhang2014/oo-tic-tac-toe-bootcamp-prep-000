febb7b95ffd6265728753153759e9928b9552122
Alex Zhang <alexgzhang92@gmail.com>
2018-05-21 00:36:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,12 @@ class TicTacToe
     end
   end
   
-  
+  def valid_move?(index)
+    if index.between?(0, 8) && !position_taken?(index)
+      return true
+    else
+      return false
+    end
+  end
   
 end
